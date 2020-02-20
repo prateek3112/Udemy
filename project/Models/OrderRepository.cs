b8650ff61d0fa0c22da96e078d2b0d5f5dad1aa0
Prateek Guglani <INT173@xceedance.com>
2020-02-20 17:06:06 +0530

    first commit

---
+++ project/Models/OrderRepository.cs
@@ -0,0 +1,45 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace project.Models
+//{
+//    public class OrderRepository : IOrderRepository
+//    {
+//        private readonly AppDbContext _appDbContext;
+      
+
+
+//        public OrderRepository(AppDbContext appDbContext, ShoppingCart shoppingCart)
+//        {
+//            _appDbContext = appDbContext;
+//            _shoppingCart = shoppingCart;
+//        }
+
+
+//        public void CreateOrder(Order order)
+//        {
+//            order.OrderPlaced = DateTime.Now;
+
+//            _appDbContext.Orders.Add(order);
+
+//            var shoppingCartItems = _shoppingCart.ShoppingCartItems;
+
+//            foreach (var shoppingCartItem in shoppingCartItems)
+//            {
+//                var orderDetail = new Orderdetails()
+//                {
+//                    Amount = shoppingCartItem.Amount,
+//                    PieId = shoppingCartItem.Pie.PieId,
+//                    OrderId = order.OrderId,
+//                    Price = shoppingCartItem.Pie.Price
+//                };
+
+//                _appDbContext.OrderDetails.Add(orderDetail);
+//            }
+
+//            _appDbContext.SaveChanges();
+//        }
+//    }
+//}
